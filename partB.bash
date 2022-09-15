c65175cb27e38c4f5909cc79939cc9e749e5d7d1
Matthew Munro (mam552) <mam552@usask.ca>
2022-09-15 15:56:01 -0600

    Tentative final draft for bash script.

--- partB.bash
+++ partB.bash
@@ -9,6 +9,34 @@
 
 #!/bin/bash
 
+if [ -z $1 ]
+then
+    printf "Error: Provide an argument., eg: ./partB.bash <executable>\n"
+    exit 0
+fi
+
+# Test to confirm the argument given corresponds to a real binary.
+case $1 in
+    partA1)
+    ;;
+    partA2)
+    ;;
+    partA3)
+    ;;
+    partA4)
+    ;;
+    *)
+        printf "Error: Argument $1 failed to match any known executables\n"
+        exit 0
+esac 
+
+# Validate we are on correct platform for partA1
+if [ "$1" = "partA1" ] & [ "$OS" != "Windows_NT" ]
+then 
+    printf "Error: Cannot run partA1 on Linux!\n"
+    exit 0
+fi
+
 printf "If executing interactively, please enter one set of "
 printf "arguments for each run. Enter quit or q to end the loop\n"
 
@@ -18,13 +46,14 @@ read line
 while [ $? -eq 0 ]
 do
     # Bail early if interactive user chooses to quit.
-    if [ $line = "q" ] || [ $line = "quit" ]
+    if [ "$line" = "q" ] | [ "$line" = "quit" ]
     then 
         exit 0
     fi
-    printf "$line\n" 
+
+    ./$1 $line
     read line
 done
 
-# one more time, since the last line had eof.
-printf "$line\n"
+# One last time, because this last line had the eof
+./$1 $line
