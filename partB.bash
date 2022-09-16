99144cf9aa07d13adb0d114f9435fd144557329e
Matthew Munro (mam552) <mam552@usask.ca>
2022-09-16 10:33:59 -0600

    Modify bash script to perform error checking.

--- partB.bash
+++ partB.bash
@@ -9,7 +9,8 @@
 
 #!/bin/bash
 
-if [ -z $1 ]
+binary="$1"
+if [ -z "$binary" ]
 then
     printf "Error: Provide an argument., eg: ./partB.bash <executable>\n"
     exit 0
@@ -26,34 +27,97 @@ case $1 in
     partA4)
     ;;
     *)
-        printf "Error: Argument $1 failed to match any known executables\n"
-        exit 0
+        printf "Error: Argument $1 failed to match any known executables!\n"
+        exit 1
 esac 
 
 # Validate we are on correct platform for partA1
-if [ "$1" = "partA1" ] & [ "$OS" != "Windows_NT" ]
+if [ "$binary" = "partA1" ] & [ "$OS" != "Windows_NT" ]
 then 
     printf "Error: Cannot run partA1 on Linux!\n"
-    exit 0
+    #exit 1
 fi
+
+# Used to confirm the user input for the executable is valid.
+validate_arg() {
+    # redirect stderr to stdout so we can catch it in this var
+    # if there's a way to catch stderr into a var directly,
+    # I am not aware of it.
+    err=$(test "$1" -lt 0 2>&1)
+    if [ ! -z "$err" ] 
+    then 
+        printf "Error: Could not understand non-nunmeric arg $1...\n"
+        return 1
+    fi
+
+    if [ $1 -lt 0 ]
+    then 
+        printf "Error: Input $1 must be non-negative...\n"
+        return 1
+    fi
+
+    return 0
+}
 
 printf "If executing interactively, please enter one set of "
 printf "arguments for each run. Enter quit or q to end the loop\n"
 
 read line
-
 # Continue while we havent hit an eof
 while [ $? -eq 0 ]
 do
     # Bail early if interactive user chooses to quit.
-    if [ "$line" = "q" ] | [ "$line" = "quit" ]
+    if [ "$line" = "q" ] || [ "$line" = "quit" ]
     then 
+        printf "Interactive user chose to quit. Goodbye.\n"
         exit 0
     fi
 
-    ./$1 $line
+    # Grab each input seperately, so we can test
+    # for invalid input.
+    threads=$(echo "$line" | cut -d " " -f 1)
+    validate_arg $threads
+    rc1=$?
+    deadline=$(echo "$line" | cut -d " " -f 2)
+    validate_arg $deadline
+    rc2=$?
+    size=$(echo "$line" | cut -d " " -f 3)
+    validate_arg $size
+    rc3=$?
+
+    if [ $rc1 = 1 ] || [ $rc2 = 1 ] || [ $rc3 = 1 ]
+    then
+        printf "Error parsing input, skipping this input...\n"
+        # Don't forget to read a new line, or we end up
+        # in an infinite loop.
+        read line
+        continue
+    fi
+    ./$binary $line
     read line
 done
 
-# One last time, because this last line had the eof
-./$1 $line
+# Do this one more time, because we hit a line with eof.
+# does bash have some kind of do while? 
+threads=$(echo "$line" | cut -d " " -f 1)
+validate_arg $threads
+rc1=$?
+deadline=$(echo "$line" | cut -d " " -f 2)
+validate_arg $deadline
+rc2=$?
+size=$(echo "$line" | cut -d " " -f 3)
+validate_arg $size
+rc3=$?
+
+if [ $rc1 = 1 ] || [ $rc2 = 1 ] || [ $rc3 = 1 ]
+then
+    printf "Error parsing input, skipping this input...\n"
+    printf "End of file reached. Goodbye.\n"
+    # Don't forget to read a new line, or we end up
+    # in an infinite loop.
+    exit 0
+fi
+
+./$binary $line
+printf "End of file reached. Goodbye.\n"
+exit 0
