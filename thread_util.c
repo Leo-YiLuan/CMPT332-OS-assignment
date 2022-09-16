b3740097e43f63187f9c7317d71d70228a67f87b
Matthew Munro (mam552) <mam552@usask.ca>
2022-09-16 14:00:50 -0600

    Fix incorrect error checking in arg_parse.

--- thread_util.c
+++ thread_util.c
@@ -15,7 +15,7 @@ int parse_args(int argc, char **argv, int *outThreads, int *outDeadline,
     *outDeadline = (int)strtol(argv[2], NULL, 10);
     *outSize = (int)strtol(argv[3], NULL, 10);
 
-    if (*outThreads <= 0 || *outDeadline <= 0 || *outSize <= 0) {
+    if (*outThreads < 0 || *outDeadline < 0 || *outSize < 0) {
         fprintf(stderr, "Error: Arguments supplied must be non-negative.");
         return 0;
     }
