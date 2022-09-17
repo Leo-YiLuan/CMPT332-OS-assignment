b10928aa74afa219c68d9c202ce852fe3cce64fe
Yi Luan (yil160) <yil160@usask.ca>
2022-09-17 15:30:26 -0600

    upload design for part C and add identification to most files

--- list.h
+++ list.h
@@ -1,3 +1,15 @@
+/*
+# NAME: Matthew Munro
+# NSID: mam552
+# STUDENT NUMBER: 11291769
+# NAME: Yi Luan
+# NSID: yil160
+# STUDENT NUMBER: 11253856
+# CMPT 332 2022
+# A1 Phase 1
+*/
+
+
 #ifndef __LIST_H__
 #define __LIST_H__
 
@@ -5,6 +17,8 @@ typedef struct _node NODE;
 struct NODE {
 	void *item;
 	NODE *next;
+    NODE *prev;
+    int nodeSize
 };
 
 typedef struct _list
@@ -15,9 +29,6 @@ typedef struct _list
     int listCount;
     int listSize;
 }LIST;
-
-
-
 
 LIST *ListCreate();
 
@@ -41,7 +52,7 @@ int ListAppend(LIST *list, void *item);
 
 int ListPrepend(LIST *list, void *item);
 
-void *ListRemove(LIST *list, void *item);
+void *ListRemove(LIST *lists);
 
 void ListConcat(LIST *list, LIST *list2);
 
---
+++ mytestlist.c
@@ -0,0 +1,18 @@
+/*
+# NAME: Matthew Munro
+# NSID: mam552
+# STUDENT NUMBER: 11291769
+# NAME: Yi Luan
+# NSID: yil160
+# STUDENT NUMBER: 11253856
+# CMPT 332 2022
+# A1 Phase 1
+*/
+#include "list.h"
+#include <stdio.h>
+
+int main(int argc, char **argv){
+    
+    LIST *newList = ListCreate();
+
+}
