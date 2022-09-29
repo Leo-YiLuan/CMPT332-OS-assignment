138bf39307a2b75642aae017ac8e03491810859b
Matthew Munro (mam552) <mam552@usask.ca>
2022-09-29 12:30:28 -0600

    Further work on A3.

--- partA3.c
+++ partA3.c
@@ -10,6 +10,8 @@
 */
 #include <stdio.h>
 #include <stdlib.h>
+#include <signal.h>
+#include <time.h>
 #include <pthread.h>
 #include "fib.h"
 #include "thread_util.h"
@@ -26,15 +28,30 @@ size_t PA_GetUID() {
 void *myThread(void *param)
 {
   int i;
+  time_t start = 0;
+  time_t end = 0;
+  time_t dur = 0;
+  time(&start);
+  for (i = 0; i < threadCount; i++) {
+    if (countArr[i].uid == PA_GetUID()) {
+      countArr[i].startTime = start;
+    }
+  }
 
   /* call fib() with given size */
   for(i=1;i<=*(int*)param;i++){
   	fib(i);
   }
+  time(&end);
+  dur = end - start;
 
-  /* skeleton program */
-  printf("Thread id: %ld, fib(%d) to fib(1) is called\n",pthread_self(),
-  *(int*)param);
+  for (i = 0; i < threadCount; i++) {
+    if (countArr[i].uid == PA_GetUID()) {
+      /* skeleton program */
+      printf("Thread id: %ld, fib(%d) to fib(1) is called with %ld invocations in %d sec.\n",pthread_self(),
+      *(int*)param, countArr[i].count, dur);
+    }
+  }
 
   pthread_exit(NULL);
 }
@@ -56,13 +73,27 @@ int main(int argc,char **argv)
     /* Create threads */
     for (i=0;i<threads;i++){
         pthread_t thread;
+        threadCount += 1;
         pthread_create( &thread, NULL, myThread, &size);
         countArr[i].uid = thread;
     }
 
-    /* wait all thread finish */
+    sleep(deadline);
+
+    /* kill threads past deadline */
+    /* Bug, not currently working. Everything else is. */
      for (i = 0; i < threads; i++) {
-        pthread_join(countArr[i].uid,NULL);
+        printf("Guard\n");
+        int res = pthread_kill(countArr[i].uid, SIGTERM);
+        printf("res: %d\n", res);
+        if (res != 0) {
+          time_t end = 0;
+          time_t dur = 0;
+          time(&end);
+          dur = end - countArr[i].startTime;
+          printf("Thread id: %ld, fib(%d) to fib(1) is called with %ld invocations in %d sec.\n",pthread_self(),
+          size, countArr[i].count, dur);
+        }
      }
 
      return 0;
