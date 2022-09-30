4c986df47990ec57d7674468661d4a1317cecb52
Matthew Munro (mam552) <mam552@usask.ca>
2022-09-30 11:17:10 -0600

    Rewrite tokenizer to use strtok.

--- shell.c
+++ shell.c
@@ -21,8 +21,6 @@ int main() {
     char **tokenArr = malloc(maxTokenCount * sizeof(char*));
     size_t cmdSize = 0;
     size_t tokenIndex = 0;
-    char *start = NULL;
-    char *charPos = NULL;
     char *path[] = {"/usr/bin/","","",""};
     /*char *argv[] = {"ls",NULL};*/
     char *concatCommand;
@@ -31,61 +29,41 @@ int main() {
     int id;
     char *test[] = {NULL};
 
-    printf("> ");
     while (1) {
+        char *strtokRes = NULL;
         /* Resetting some vars in prep for the next command */
-        start = NULL;
-        charPos = NULL;
         tokenIndex = 0;
         cmdSize = 0;
         printf("> ");
         /* Grab the next command from stdin */
-        getline(&command, &cmdSize, stdin);
-        charPos = command;
-        start = charPos;
+        getline(&command, &cmdSize, stdin);;
 
-        /* Main tokenizer loop */
-        while (1) {
+        tokenIndex = 0;
+        /* This should never happen, but just to be safe...*/
+        if (!command) { break; }
 
-            /* This should never happen, but just to be safe...*/
-            if (!command) { break; }
-
-            /*
-             Our two delimiters - \n is used because it mar        size_t pathLen = strlen(path[0]);
-        size_t cmdLen = strlen(tokenArr[0]);ksargv
-             that the user hit enter and submitted a command.
-             Is it safe to always assume a command ends with a \n?
-             */
-            if (*charPos != ' ' && *charPos != '\n') {
-                /* Seek through the char pointer by one space. */
-                charPos += 1;
-            } else {
-                /* We've found the end of a single token - null terminate it */
-                *charPos = '\0';
-                charPos += 1;
-                /* ignore additional whitespace */
-                while (*charPos == ' ') {
-                    charPos += 1;
-                }
-
-                /* Count is one plus current index. */
-                if (tokenIndex + 1 > maxTokenCount) {
-                    maxTokenCount *= 2;
-                    tokenArr = realloc(tokenArr, maxTokenCount * sizeof(char *));
-                    if (!tokenArr) {
-                        printf("Fatal error allocating memory, must quit...\n");
-                        return -1;
-                    }
-                }
-                /* Add our completed token */
-                tokenArr[tokenIndex] = start;
-                start = charPos;
-                tokenIndex += 1;
+        /* Main tokenizer code. */
+        tokenArr[0] = strtok(command, " ");
+        if (!tokenArr[0]) {
+            /* There were no tokens, skip. */
+            continue;
+        }
+        while ((strtokRes = strtok(NULL, " "))) {
+            tokenIndex += 1;
+            if (tokenIndex + 1 > maxTokenCount) {
+                maxTokenCount *= 2;
+                tokenArr = realloc(tokenArr, maxTokenCount * sizeof(char*));
             }
+            tokenArr[tokenIndex] = strtokRes;
+        }
+        /* Our token array must be null-terminated, according to man exec. */
+        tokenIndex += 1;
+        if (tokenIndex + 1 > maxTokenCount) {
+            maxTokenCount += 1;
+            tokenArr = realloc(tokenArr, maxTokenCount * sizeof(char*));
+        }
+        tokenArr[tokenIndex] = NULL;
 
-            /* We've reached the end of the command string, done... */
-            if (*charPos == '\0') { break; }
-        };
 
         /* TODO: Probably where we would fork processes and set up pipes. */
         /* For now, just print tokens back to stdout... */
@@ -94,7 +72,7 @@ int main() {
             for (i = 0; i < tokenIndex; i++) {
                 printf("%s\n", tokenArr[i]);
             }
-        }4
+        }
 
         pathLen = strlen(path[0]);
         cmdLen = strlen(tokenArr[0]);
@@ -113,7 +91,6 @@ int main() {
         /* Free the old cmd string now that we are done with it. */
         free(command);
         command = NULL;
-        /* Reprint the prompt in preparation for next user command */
     }
 
     /* Shouldn't matter, but might as well do it. */
