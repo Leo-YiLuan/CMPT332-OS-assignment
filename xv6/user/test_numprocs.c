d04e1b4c049d2db3de2c9993cc565cfb173fa4f0
Yi Luan (yil160) <yil160@usask.ca>
2022-10-02 12:58:46 -0600

    partD test added

--- xv6/user/test_numprocs.c
+++ xv6/user/test_numprocs.c
@@ -4,44 +4,92 @@
 int i;
 
 int testNumprocs(int n){
-  int childrenID[64];
-  int current_runnable;
-  int tempid=0;
-
-  for (i = 0; i < n; i++) {
-    printf("forked\n" );
-    tempid = fork();
-
-    if (tempid==0) {
-      while (1);
-    }else if (tempid == -1) {
-      printf("Fail to create new process\n" );
-    }else{
-      childrenID[i] = tempid;
-    }
-  }
-
-  current_runnable = numprocs();
-
-  for (i = 0; i < n; i++) {
-    if (kill(childrenID[i])==-1) {
-      printf("Fail to kill: %d\n",childrenID[i]);
-    }
-      childrenID[i]=0;
-
+  	int tempid;
+	for (i = 0; i < n; i++) {
+		tempid = fork();
+		if (tempid==0) {
+			while(1);
+   	 	}else if (tempid == -1) {
+   			printf("Fail to create new process\n" );
+   		}
+  	}	
+  	return numprocs();
 }
 
-  return current_runnable;
 
-}
-
-int
-main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-
-  printf("10: %d\n", testNumprocs(10));
-  printf("12: %d\n", testNumprocs(12));
-  printf("21: %d\n", testNumprocs(21));
-
-  return 0;
+    int procNum=0;
+    int expect=0;
+    int result=testNumprocs(procNum);
+	int totalProcs = procNum;
+    if(result!=expect){
+        printf("Error: test of %d processes expect to get: %d, but"
+		" get: %d\n", totalProcs, expect, result);
+ 	}else{
+		printf("test is passed when number of RUNNABLE process is %d\n",
+		totalProcs);
+	}
+	
+    procNum=5;
+    expect+=5;
+    result=testNumprocs(procNum);
+	totalProcs += procNum;
+    if (result!=expect){
+         printf("Error: test of creating: %d more processes expect to get: %d,"
+		" but get: %d\n", totalProcs, expect, result);
+ 	}
+	else{
+		printf("test is passed when number of RUNNABLE process is %d\n",
+		totalProcs);
+	}
+    procNum=10;
+    expect+=10;
+    result=testNumprocs(procNum);
+	totalProcs += procNum;
+    if(result!=expect){
+         printf("Error: test of creating: %d more processes expect to get: %d,"
+		" but get: %d\n", totalProcs, expect, result);
+ 	}
+	else{
+		printf("test is passed when number of RUNNABLE process is %d\n",
+		totalProcs);
+	}
+    procNum=20;
+    expect+=20;
+    result=testNumprocs(procNum);
+	totalProcs += procNum;
+    if(result!=expect){
+         printf("Error: test of creating: %d more processes expect to get: %d,"
+		" but get: %d\n", totalProcs, expect, result);
+ 	}
+	else{
+		printf("test is passed when number of RUNNABLE process is %d\n",
+		totalProcs);
+	}
+    procNum=11;
+    expect+=11;
+    result=testNumprocs(procNum);
+	totalProcs += procNum;
+    if(result!=expect){
+         printf("Error: test of creating: %d more processes expect to get: %d,"
+		" but get: %d\n", totalProcs, expect, result);
+ 	}
+	else{
+		printf("test is passed when number of RUNNABLE process is %d\n",
+		totalProcs);
+	}
+    procNum=10;
+    expect+=10;
+    result=testNumprocs(procNum);
+	totalProcs += procNum;
+    if(result!=expect){
+         printf("Error: test of creating: %d more processes expect to get: %d,"
+		" but get: %d\n", totalProcs, expect, result);
+ 	}
+	else{
+		printf("test is passed when number of RUNNABLE process is %d\n",
+		totalProcs);
+	}
+	 return 0;
 }
