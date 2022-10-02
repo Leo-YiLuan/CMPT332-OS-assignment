4b0ce46b00ded659a9ef28371874878bed5f7409
Yi Luan (yil160) <yil160@usask.ca>
2022-10-01 18:18:49 -0600

    PartD testing frame is done

--- xv6/user/test_numprocs.c
+++ xv6/user/test_numprocs.c
@@ -1,18 +1,36 @@
 #include "kernel/types.h"
 #include "user.h"
 
+int i;
+
 int testNumprocs(int n){
-  int *childrenID = malloc(sizeof(int)*n);
+  int childrenID[64];
   int current_runnable;
-  int i;
+  int tempid=0;
+
   for (i = 0; i < n; i++) {
-    if (fork()==0) {
-      childrenID[i] = getpid();
+    printf("forked\n" );
+    tempid = fork();
+
+    if (tempid==0) {
       while (1);
+    }else if (tempid == -1) {
+      printf("Fail to create new process\n" );
+    }else{
+      childrenID[i] = tempid;
     }
   }
+
   current_runnable = numprocs();
-  printf("%d|||\n", current_runnable);
+
+  for (i = 0; i < n; i++) {
+    if (kill(childrenID[i])==-1) {
+      printf("Fail to kill: %d\n",childrenID[i]);
+    }
+      childrenID[i]=0;
+
+}
+
   return current_runnable;
 
 }
@@ -21,7 +39,9 @@ int
 main(int argc, char *argv[])
 {
 
-  testNumprocs(5);
+  printf("10: %d\n", testNumprocs(10));
+  printf("12: %d\n", testNumprocs(12));
+  printf("21: %d\n", testNumprocs(21));
 
   return 0;
 }
