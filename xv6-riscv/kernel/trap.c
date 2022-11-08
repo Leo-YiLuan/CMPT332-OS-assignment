ec87dac6d3f1fcfb4c078da57acde046c827ba36
Matthew Munro (mam552) <mam552@usask.ca>
2022-11-08 14:52:43 -0600

    Implement trap handler.
    Currently loops infinitely, not sure why.

--- xv6-riscv/kernel/trap.c
+++ xv6-riscv/kernel/trap.c
@@ -65,6 +65,63 @@ usertrap(void)
     intr_on();
 
     syscall();
+  } else if (r_scause() == 15) {
+    /* CMPT 332 GROUP 22 Change, Fall 2022 */
+    // Handle a page fault on data write/store
+
+    if(killed(p))
+      exit(-1);
+
+        intr_on();
+    
+    // Get faulting address and pagetable
+    uint64 va = r_stval();
+    uint64 vaAligned = PGROUNDDOWN(va);
+    pte_t *pte = walk(p->pagetable, va, 0);
+    uint64 pa = PTE2PA(*pte);
+
+    // Check if the faulting address is valid
+    if((*pte & PTE_V) == 0) {
+      // Invalid virtual address!
+      printf("usertrap(): Page fault on invalid address. pid = %d, addr = %p\n", p->pid, va);
+      setkilled(p);
+    } else {
+      printf("Faulting address at va: %p, page-aligned: %p, phys addr: %p, pid: %d\n", va, vaAligned, pa, p->pid);
+    }
+    printf("Is writable: %d\n", PTE_FLAGS(*pte) & PTE_W);
+
+    if (page_ref_count(pa) > 1) {
+      // Create a new physical page.
+      char *newPage;
+      if((newPage = kalloc()) == 0) {
+        panic("usertrap page fault cannot alloc memory");
+      }
+
+      // Move data into new page. 
+      memmove(newPage, (char*)pa, PGSIZE);
+
+      // Unmap old read-only page from page table.
+      // uvmunmap handles the decrementing for us.
+      uvmunmap(p->pagetable, vaAligned, 1, 0);
+
+      // Map the newly constructed copy into the same spot, now with writeable perms.
+      mappages(p->pagetable, vaAligned, PGSIZE, (uint64)newPage, PTE_FLAGS(*pte) | PTE_W | PTE_R);
+      // Flush TLB
+      sfence_vma();
+      printf("Mapped new page, writeable: %d\n", PTE_FLAGS(*walk(p->pagetable, va, 0)) & PTE_W);
+    } else {
+      // This must be the parent, as they have only 1 ref left.
+      // We can just modify the permissions to let them write to it. 
+      printf("Page has only 1 refcount, simply enable writing perm.\n");
+      uint flags = PTE_FLAGS(*pte);
+      flags = flags | PTE_W;
+      *pte = PA2PTE(pa) | flags;
+      // Flush TLB
+      sfence_vma();
+    }
+
+    printf("Completed page fault. sepc: %p\n", p->trapframe->epc);
+
   } else if((which_dev = devintr()) != 0){
     /* ok */
   } else {
